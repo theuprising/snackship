cbd588876d1a63ad177b5409504996d8805a0752
Andrew Monks <a@monks.co>
2016-10-17 01:59:11 -0500

    require().default

--- snackpack/config/defaults.js
+++ snackpack/config/defaults.js
@@ -1,7 +1,7 @@
 import { BannerPlugin, DefinePlugin } from 'webpack'
 
 const injection = {
-  'REQUIRE_CONFIG': 'require(path.resolve(process.cwd(), commander.configFile))'
+  'REQUIRE_CONFIG': 'require(path.resolve(commander.configFile)).default'
 }
 console.log('INJECTION', injection)
 const plugins = [
